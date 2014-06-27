b88a6b66b1ff116c0311ef3851cb87ac3b1f5a3c
Patrick Moore <patmoore@farreach.es>
2014-06-27 08:42:14 -0700

    run scripts put in background

--- run-node-inspector.sh
+++ run-node-inspector.sh
@@ -2,6 +2,6 @@
 # Port to host the inspector
 #    Host to listen on
 #    --web-host={String} (default: "")
-node-inspector --web-port=7003 --debug-port=7002
+node-inspector --web-port=7003 --debug-port=7002 &
 
 
