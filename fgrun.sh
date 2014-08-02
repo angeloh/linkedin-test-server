6dcff4c856386a10ce4c7ed3677488d4ad3998a7
Daniel Radetsky <dradetsky@gmail.com>
2014-08-01 18:49:13 -0700

    Add comment.

--- fgrun.sh
+++ fgrun.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# this script is for people (e.g. dmr) who don't like the run-in-background 
+# behavior of the standard run.sh
 
 echo "mongo will be on port 7001"
 NODE_OPTIONS=--debug=7002 mrt -p 7000 run
