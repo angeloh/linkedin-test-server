7bd6f5a0ac6622d7c1986d296d7823fc5f9eb9fa
Daniel Radetsky <dradetsky@gmail.com>
2015-05-18 14:49:39 -0700

    cleanup

--- intercom-test-server/src/server/lib/managers/usersManager.js
+++ intercom-test-server/src/server/lib/managers/usersManager.js
@@ -1,7 +1,9 @@
 Meteor.startup(function() {
     _.extend(UsersManagerType.prototype, {
         createIntercomUserResponseObject: function(intercomUser) {
-            var response = _.omit(intercomUser.toJSONValue(), '_id','tagIds', 'lastModifiedAt', 'createdAt');
+            var response = _.omit(intercomUser.toJSONValue(),
+                '_id','tagIds', 'lastModifiedAt', 'createdAt'
+            );
             response.id = intercomUser.id;
             response.created_at = intercomUser.createdAt;
             if ( intercomUser.tagIds) {
@@ -16,21 +18,42 @@ Meteor.startup(function() {
 
         createUserResponseMethod: function(request) {
             var thatManager = this.thatManager;
-            /*
-            user_id	yes if no email	a unique string identifier for the user. It is required on creation if an email is not supplied.
-                email	yes if no user_id	the user’s email address. It is required on creation if a user_id is not supplied.
-                id	no	The id may be used for user updates.
-                signed_up_at	no	The time the user signed up
-            name	no	The user’s full name
-            last_seen_ip	no	An ip address (e.g. “1.2.3.4”) representing the last ip address the user visited your application from. (Used for updating location_data)
-            custom_attributes	no	A hash of key/value pairs containing any other data about the user you want Intercom to store.*
-            last_seen_user_agent	no	The user agent the user last visited your application with.
-            companies	no	Identifies the companies this user belongs to.
-                last_request_at	no	A UNIX timestamp representing the date the user last visited your application.
-                unsubscribed_from_emails	no	A boolean value representing the users unsubscribed status. default value if not sent is false.
-                update_last_request_at	no	A boolean value, which if true, instructs Intercom to update the users’ last_request_at value to the current API service time in UTC. default value if not sent is false.
-                new_session	no	A boolean value, which if true, instructs Intercom to register the request as a session.
-                */
+            /* key: param, (required), dox
+            user_id (yes if no email a unique string identifier for the user.) It is required on
+            creation if an email is not supplied.
+
+            email (yes if no user_id the user’s email address.) It is required on creation if a
+            user_id is not supplied.
+
+            id (no) The id may be used for user updates.
+
+            signed_up_at (no) The time the user signed up
+
+            name (no) The user’s full name
+
+            last_seen_ip (no) An ip address (e.g. “1.2.3.4”) representing the last ip address the user
+            visited your application from. (Used for updating location_data)
+
+            custom_attributes (no) A hash of key/value pairs containing any other data about the user
+            you want Intercom to store.*
+
+            last_seen_user_agent (no) The user agent the user last visited your application with.
+
+            companies (no) Identifies the companies this user belongs to.
+
+            last_request_at (no) A UNIX timestamp representing the date the user last visited your
+            application.
+
+            unsubscribed_from_emails (no) A boolean value representing the users unsubscribed
+            status. default value if not sent is false.
+
+            update_last_request_at (no) A boolean value, which if true, instructs Intercom to
+            update the users’ last_request_at value to the current API service time in
+            UTC. default value if not sent is false.
+
+            new_session (no) A boolean value, which if true, instructs Intercom to register the
+            request as a session.
+            */
             var intercomUser;
             var body = request.body;
             var intercomId = body.id;
@@ -44,7 +67,7 @@ Meteor.startup(function() {
                 if ( intercomUser == null ) {
                     return {
                         statusCode: 404
-                    }
+                    };
                 }
             } else {
                 intercomUser = IntercomUser.findOneByUser_id(userId);
@@ -90,7 +113,7 @@ Meteor.startup(function() {
                 if ( intercomUser == null ) {
                     return {
                         statusCode: 404
-                    }
+                    };
                 }
             } else {
                 if (userId) {
@@ -102,7 +125,7 @@ Meteor.startup(function() {
                 if (intercomUser == null) {
                     return {
                         statusCode: 404
-                    }
+                    };
                 }
             }
 
@@ -117,4 +140,4 @@ Meteor.startup(function() {
             }
         }
     });
-})
+});
