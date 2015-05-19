db1ae90e4b812203945f7fcfb1d6558463219671
Daniel Radetsky <dradetsky@gmail.com>
2015-05-19 15:52:07 -0700

    script in src

---
+++ intercom-test-server/src/fgrun.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# this script is for people (e.g. dmr) who don't like the run-in-background 
+# behavior of the standard run.sh
+ 
+echo "mongo will be on port 8001"
+NODE_OPTIONS=--debug=8002 meteor -p 8000 run
