ccf0a99f9699f072c4d2944b9dc2c48cae8b5587
Patrick Moore <patmoore@farreach.es>
2015-05-28 11:31:37 -0700

    bgrun.sh

--- bgrun.sh
+++ bgrun.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export METEOR_OFFLINE_CATALOG=1
+echo "Fast start...to avoid multiple servers trying to update the meteor versions db."
 servers=$*
 if [ -z "$servers" ] ; then
     servers=*-test-server
@@ -6,4 +8,5 @@ fi
 for x in $servers ; do
     echo $x ...
     ( cd $x; ./bgrun.sh )
+    sleep 1
 done
